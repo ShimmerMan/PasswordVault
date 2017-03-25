f10409df78c5bc3e8512779296156fe88816d493
ShimmerMan <jeremy.t.larsen@gmail.com>
2017-03-25 20:09:07 +1000

    Added new view package along with creating a Gui Form for the landing view

---
+++ src/main/java/com/shimmerman/passwordvault/view/LandingView.java
@@ -0,0 +1,19 @@
+package com.shimmerman.passwordvault.view;
+
+/**
+ * Created by Jeremy on 3/25/2017.
+ */
+
+/**
+ * @author Jeremy
+ */
+public class LandingView {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
