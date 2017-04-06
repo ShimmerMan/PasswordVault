9e981e500644b1c2be8bf3719d626e98bf705622
ShimmerMan <jeremy.t.larsen@gmail.com>
2017-04-06 22:04:44 +1000

    Created a logger for the PasswordVault class.

--- src/main/java/com/shimmerman/passwordvault/PasswordVault.java
+++ src/main/java/com/shimmerman/passwordvault/PasswordVault.java
@@ -1,6 +1,13 @@
 package com.shimmerman.passwordvault;
 
+/**
+ *  Using the log4j Logger for logging errors, warnings, etc.
+ */
+import org.apache.log4j.Logger;
+
 public class PasswordVault {
+
+    final static Logger logger = Logger.getLogger(PasswordVault.class);
 
     public static void main(String[] agrs) {
         System.out.println("Hello world!");
