0a9c45425c6ec0a8c637cb8a1244cab2d8ccb5c7
Carl Hansen <carl_hansen@hotmail.com>
2017-04-23 15:15:48 +1000

    Changed JUint dependency and updated tests to match.

--- src/test/java/com/shimmerman/passwordvault/test/model/DataManagerTest.java
+++ src/test/java/com/shimmerman/passwordvault/test/model/DataManagerTest.java
@@ -1,36 +1,29 @@
 package com.shimmerman.passwordvault.test.model;
 
 import com.shimmerman.passwordvault.model.DataManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-class DataManagerTest {
-    @BeforeEach
-    void setUp() {
 
-    }
+public class DataManagerTest {
 
-    @AfterEach
-    void tearDown() {
-
-    }
 
     @Test
-    void createAndShutdownTest() {
+    public void createAndShutdownTest() {
         try {
             // Create the database
             boolean isCreated = DataManager.createConnection();
-            assertTrue(isCreated, "Could not create successfully");
+            assertTrue("Could not create successfully", isCreated);
 
             // Shutdown the database
             boolean isShutdown = DataManager.shutdown();
-            assertTrue(isCreated, "Could not shutdown successfully");
-            
+            assertTrue("Could not shutdown successfully", isCreated);
+
         } catch (Exception e) {
-            fail("Failed because of thrown exception", e);
+            fail("Failed because of thrown exception");
+            e.printStackTrace();
         }
     }
 
